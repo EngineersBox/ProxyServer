1c1cba3d279b5f0e523c0e40b0a9cafc86d623d8
EngineersBox <toastcraft.info@gmail.com>
2019-10-13 15:51:43 +1100

    Added logging class, improved flag system

--- Proxy.py
+++ Proxy.py
@@ -1,10 +1,25 @@
 import socket, os, parser
 from threading import Thread
 from uuid import uuid4
+from pwn import *
 
 '''
 TCP Network Proxy
 '''
+
+class logging:
+
+    def status(msg):
+        print("[" + text.magenta("x") + "] " + msg)
+
+    def error(msg):
+        print("[" + text.on_red("Error") + "] " + str(msg))
+
+    def listelem(marker, msg):
+        print("(" + text.bold_blue(marker) + ") " + str(msg))
+
+    def listinfo(marker, msg):
+        print("(" + text.bold_green(marker) + ") " + str(msg))
 
 class ProxyToServer(Thread):
 
@@ -23,9 +38,9 @@ class ProxyToServer(Thread):
             if data:
                 try:
                     reload(parser)
-                    parser.parse(data, self.port, "server")
+                    parser.parse(data, self.port, "Server")
                 except Exception as e:
-                    print("server[{}]".format(self.port), e)
+                    log.warn("Server[{}]".format(self.port), e)
                 self.game.sendall(data)
 
 class ClientToProxy(Thread):
@@ -38,7 +53,7 @@ class ClientToProxy(Thread):
         self.uid = uid
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((host, port))
+        sock.bind((host, int(port)))
         sock.listen(1)
         self.game, addr = sock.accept()
 
@@ -48,9 +63,9 @@ class ClientToProxy(Thread):
             if data:
                 try:
                     reload(parser)
-                    parser.parse(data, self.port, "client")
+                    parser.parse(data, self.port, "Client")
                 except Exception as e:
-                    print("client[{}]".format(self.port), e)
+                    log.warn("Client[{}]".format(self.port), e)
                 self.server.sendall(data)
 
 class Proxy(Thread):
@@ -65,26 +80,35 @@ class Proxy(Thread):
 
     def run(self):
         while True:
-            print("[proxy({})] setting up".format(self.port))
+            logging.status("[Proxy({})] Initialising...".format(self.port))
             self.ctp = ClientToProxy(self.from_host, self.port, self.ctpuid)
             self.pts = ProxyToServer(self.to_hist, self.port, self.ptsuid)
-            print("[proxy({})] connection established".format(self.port))
+            log.success("[Proxy({})] Connection established".format(self.port))
             self.ctp.server = self.pts.server
             self.pts.client = self.ctp.client
 
             self.ctp.start()
             self.pts.start()
 
+    ## TODO: Fix close() method to actually stop threads and not just return a weird number
     def close(self):
         super()._stop()
         super()._delete()
-        print(super().isAlive())
 
     def getClientUid(self):
         return self.ctpuid
 
     def getServerUid(self):
         return self.ptsuid
+
+    def getInterface(self):
+        return self.from_host
+
+    def getServer(self):
+        return self.to_host
+
+    def getPort(self):
+        return self.port
 
 if __name__ == "__main__":
     master_server = Proxy("0.0.0.0", "192.168.178.54", 3333)
@@ -100,9 +124,8 @@ if __name__ == "__main__":
             "help": "Display this list",
             "setserver": "Open a new connection to a server: \"setserver <interface> <ip> <port>\"",
             "setclient": "Open a new connection to a client: \"setclient <interface> <ip> <port>\"",
-            "client": "Apply flags to client: [-ca: close all] [-ci <id>: close with id]",
-            "server": "Apply flags to server: [-c: close server]",
-            "clientids": "Display active client id's"
+            "client": "Apply flags to client: \n-ca: close all\n-ci <id>: close with id\n-ids: Display active client id's",
+            "server": "Apply flags to server: \n-c: close server"
     }
 
     while True:
@@ -114,18 +137,18 @@ if __name__ == "__main__":
             elif (cmd[0] == "help"):
                 print("--Commands--")
                 for key, val in cmds.items():
-                    print("[*] {} :: {}".format(key, val))
+                    log.info("[*] {} :: {}".format(key, val))
                 print("--Commands--")
             elif (cmd[0] == "setserver"):
                 if (len(cmd) != 4):
-                    print("Error: Command format is \"setserver <interface> <ip> <port>\"")
+                    log.warn("Error: Command format is \"setserver <interface> <ip> <port>\"")
                 else:
                     if (not master_server):
                         master_server = Proxy(cmd[1], cmd[2], int(cmd[3]))
                         master_server.start()
-                        print("[> Started server connection: [ip {}] [port{}]".format(cmd[2], cmd[3]))
+                        log.info("[> Started server connection: [ip {}] [port{}]".format(cmd[2], cmd[3]))
                     else:
-                        print("Error: Server connection already established, close with \"server -c\"")
+                        log.warn("Error: Server connection already established, close with \"server -c\"")
             elif (cmd[0] == "setclient"):
                 if (len(cmd) != 4):
                     print("Error: Command format is \"setclient <interface> <ip> <port>\"")
@@ -133,20 +156,20 @@ if __name__ == "__main__":
                     client = Proxy(cmd[1], cmd[2], cmd[3])
                     client.start()
                     client_servers.append(client)
-                    print("[> Started client connection: [ip {}] [port{}]".format(cmd[2], cmd[3]))
+                    log.info("[> Started client connection: [ip {}] [port{}]".format(cmd[2], cmd[3]))
             elif (cmd[0] == "client"):
                 if (len(cmd) < 2):
-                    print("Error: Command format is \"client <flags>\"")
+                    log.warn("Error: Command format is \"client <flags>\"")
                 else:
                     if ("-ca" in cmd):
                         for client in client_servers:
                             client.close()
                             client_servers.remove(clients)
-                        print("[> Closed all open clients")
+                        log.info("[> Closed all open clients")
                     if ("-ci" in cmd):
                         index = cmd.index("-ci")
                         if (index >= len(cmd) - 1):
-                            print("Error: \"-ci\" flag must have an id after it")
+                            log.warn("Error: \"-ci\" flag must have an id after it")
                         else:
                             len_pre = len(client_servers)
                             for client in client_servers:
@@ -154,23 +177,31 @@ if __name__ == "__main__":
                                     client.close()
                                     client_servers.remove(client)
                             if (len_pre == len(client_servers)):
-                                print("Error: No open client with id: {}".format(cmd[index+1]))
-                            print("[> Closed client with id: {}".format(cmd[index + 1]))
+                                log.warn("Error: No open client with id: {}".format(cmd[index+1]))
+                            log.info("[> Closed client with id: {}".format(cmd[index + 1]))
+                    if ("-i" in cmd):
+                        index = cmd.index("-i")
+                        if (index >= len(cmd) - 1):
+                            log.warn("Error: \"-ci\" flag must have an id after it")
+                        else:
+                            for client in client_servers:
+                                if (str(client.getClientUid()) == cmd[index + 1]):
+                                    log.info("[> Client " + text.bold_yellow(str(client.getClientUid())) + "\nInterface: {}\nServer: {}\nPort: {}".format(client.getInterface(), client.getServer(), client.getPort()))
+                    if ("-ids" in cmd):
+                        if (len(client_servers) < 1):
+                            log.warn("Error: No active clients")
+                        else:
+                            log.info("[> Active Clients")
+                            for i in range(0, len(client_servers)):
+                                logging.listelem(str(i), client_servers[i].getClientUid())
             elif (cmd[0] == "server"):
                 if (len(cmd) < 2):
-                    print("Error: Command format is \"server <flags>\"")
+                    log.warn("Error: Command format is \"server <flags>\"")
                 else:
                     if ("-c" in cmd):
                         master_server.close()
-                        print("[> Closed active server connection")
-            elif (cmd[0] == "clientids"):
-                if (len(client_servers) < 1):
-                    print("Error: No active clients")
-                else:
-                    print("[> Active Clients")
-                    for i in range(0, len(client_servers)):
-                        print("({})".format(i), client_servers[i].getClientUid())
+                        log.info("[> Closed active server connection")
             else:
-                print("Error: Invalid command, type \"help\" for a list of commands")
+                log.warn("Error: Invalid command, type \"help\" for a list of commands")
         except Exception as e:
-            print(e)
+            logging.error(e)
